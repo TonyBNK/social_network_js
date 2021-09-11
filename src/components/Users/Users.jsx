47e75dd5082ec90b6ecd3ed96b95af51da09e04f
TonyBNK <borisenk-anton@yandex.ru>
2021-09-11 13:19:48 +0300

    54. Used componentDidMount() instead of constructor.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,7 @@ import axios from "axios";
 import catUser from '../../img/catUser.png';
 
 export class Users extends React.Component {
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         if (this.props.users.length === 0) {
             axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
                 this.props.setUsers(response.data.items);
