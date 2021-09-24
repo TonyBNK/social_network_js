f6560c6dc412aa73b621826c38d7fb62579345a8
TonyBNK <borisenk-anton@yandex.ru>
2021-09-24 14:55:48 +0300

    71. Added ProfileStatus component using local state and setState.

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import c from './ProfileInfo.module.css';
 import {Preloader} from "../../Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus/ProfileStatus";
 
 export const ProfileInfo = (
     {
@@ -14,12 +15,13 @@ export const ProfileInfo = (
 
     return (
         <div className={c.info}>
-            <div className={c.titleImage}>
-                <img src={titleImage} alt="азгард"/>
-            </div>
+            {/*<div className={c.titleImage}>*/}
+            {/*    <img src={titleImage} alt="азгард"/>*/}
+            {/*</div>*/}
             <div className={c.avatar}>
                 <img src={profile.photos.large} alt="ava"/>
             </div>
+            <ProfileStatus/>
             <div className={c.description}>
                 <span className={c.fullName}>
                     {profile.fullName}
